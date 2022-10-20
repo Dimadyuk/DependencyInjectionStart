439c221da5f5f15f6dfd592c69ce35668fe9eda8
Dimadyuk <dimadyuk88@gmail.com>
2022-10-20 12:28:12 +0200

    lesson 187 add ViewModelsModule and 2nd ViewModel

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/datasource/ExampleLocalDataSourceImpl.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/datasource/ExampleLocalDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.example.dependencyinjectionstart.example2.data.datasource
 import com.example.dependencyinjectionstart.example2.data.database.ExampleDataBase
 import javax.inject.Inject
 
+
 class ExampleLocalDataSourceImpl @Inject constructor(
     private val dataBase: ExampleDataBase
 ): ExampleLocalDataSource {
--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -9,8 +9,8 @@ import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
-@Singleton
-@Component(modules = [DataModule::class, DomainModule::class])
+@ApplicationScope
+@Component(modules = [DataModule::class, DomainModule::class, ViewModelModule::class])
 interface ApplicationComponent {
 
 //    fun getViewModel(): ExampleViewModel
